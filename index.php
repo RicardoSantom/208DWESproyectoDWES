2a2babac5c1893d2613f0b81235e3e337e7c4045
RicardoSantom <richard1978_1@hotmail.com>
2022-12-20 07:37:04 +0100

    Ampliación documentación.

--- index.php
+++ index.php
@@ -8,9 +8,14 @@
         <meta name="author" content="Ricardo Santiago Tomé">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="icon" type="image/png" sizes="96x96" href="../webroot/images/favicon-96x96.png">
-       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="webroot/css/estilos.css"/>
         <title>Desarrollo Aplicaciones Web Entorno Servidor</title>
+        <style>
+            .sangrado{
+                margin-left: 2rem;
+            }
+        </style>
     </head>
     <body>
         <header>
@@ -21,11 +26,13 @@
             <article>
                 <ul>
                     <li><a href="doc/EstudioTema1.pdf" title="Enlace EstudioTema1.pdf" target="blank">TEMA 1:DESARROLLO WEB EN ENTORNO SERVIDOR</a><hr></li>
+                    <li class="sangrado"><a href="doc/EstudioLaravel.pdf" title="Enlace Laravel.pdf" target="blank">TEMA 1:DESARROLLO WEB EN ENTORNO SERVIDOR - Laravel</a><hr></li>
+                    <li class="sangrado"><a href="doc/Instalación y Despliegue Laravel.pptx" title="Enlace Instalación y Despliegue Laravel.pptx" target="blank">TEMA 1:DESARROLLO WEB EN ENTORNO SERVIDOR - Powerpoint Laravel</a><hr></li>
                     <li><a href="doc/EstudioTema2.pdf" title="Enlace EstudioTema2.pdf" target="blank">TEMA 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a><hr></li>
                     <li><a href="../../208DWESproyectoTema3/indexProyectoTema3.php" title="Enlace indexProyectoTema3">TEMA 3: CARACTERÍSTICAS DEL LENGUAJE PHP</a><hr></li>
                     <li><a href="../../208DWESproyectoTema4/indexProyectoTema4.php" title="Enlace indexProyectoTema4">TEMA 4: TÉCNICAS DE ACCESO A DATOS EN PHP</a><hr></li>
                     <li><a href="../../208DWESproyectoTema5/indexProyectoTema5.php" title="Enlace indexProyectoTema5">TEMA 5: DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</a><hr></li>
-                    <li style="margin-left: 2rem"><a href="../../208DWESLoginLogoffTema5/codigoPHP/login.php" title="Enlace login.php">TEMA 5: APLICACIÓN LOGINLOGOFF</a><hr></li>
+                    <li class="sangrado"><a href="../../208DWESLoginLogoffTema5/codigoPHP/login.php" title="Enlace login.php">TEMA 5: APLICACIÓN LOGINLOGOFF</a><hr></li>
                     <li><a href="../enconstruccion.html" title="Enlace en construcción">TEMA 6: PROGRAMACIÓN ORIENTADA A OBJETOS EN PHP</a><hr></li>
                     <li><a href="../enconstruccion.html" title="Enlace en construcción">TEMA 7: PROGRAMACIÓN DE SERVICIOS WEB</a><hr></li>
                     <li><a href="../enconstruccion.html" title="Enlace en construcción">TEMA 8: DESARROLLO DE APLICACIONES WEB HÍBRIDAS</a><hr></li>
