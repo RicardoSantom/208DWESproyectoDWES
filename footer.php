2b8676814a55121430732b829bc2b87a7e4c9157
RicardoSantom <richard1978_1@hotmail.com>
2022-12-20 07:41:57 +0100

    Añadido y enlazado footer.php.

---
+++ footer.php
@@ -0,0 +1,8 @@
+<
+<footer>
+    <p>2022-23  IES LOS SAUCES. Ricardo Santiago Tomé © Todos los derechos reservados</p>
+    <a href="https://github.com/RicardoSantom/208DWESLoginLogoffTema5" target="blank" id="github" title="RicardoSantom en GitHub"></a>
+    <a href="https://www.linkedin.com/in/ricardo-santiago-tom%C3%A9/" id="linkedin" title="Ricardo Santiago Tomé en Linkedim" target="_blank"></a>
+    <a href="../doc/curriculumRicardo.pdf" class="material-icons" title="Curriculum Vitae Ricardo Santiago Tomé" target="_blank" id="curriculum"><span class="material-icons md-18">face</span></a>
+    <a href="../index.html" id="enlaceSecundario" title="Enlace a Index Principal">Página principal</a>
+</footer>
--- index.php
+++ index.php
@@ -38,13 +38,7 @@
                     <li><a href="../enconstruccion.html" title="Enlace en construcción">TEMA 8: DESARROLLO DE APLICACIONES WEB HÍBRIDAS</a><hr></li>
                 </ul>
             </article>
+             <?php include_once './footer.php'; ?>
         </main>
-        <footer>
-            <p>2022-23  IES LOS SAUCES. Ricardo Santiago Tomé © Todos los derechos reservados</p>
-            <a href="https://github.com/RicardoSantom/208DWESLoginLogoffTema5" target="blank" id="github" title="RicardoSantom en GitHub"></a>
-            <a href="https://www.linkedin.com/in/ricardo-santiago-tom%C3%A9/" id="linkedin" title="Ricardo Santiago Tomé en Linkedim" target="_blank"></a>
-            <a href="../doc/curriculumRicardo.pdf" class="material-icons" title="Curriculum Vitae Ricardo Santiago Tomé" target="_blank" id="curriculum"><span class="material-icons md-18">face</span></a>
-            <a href="../index.html" id="enlaceSecundario" title="Enlace a Index Principal">Página principal</a>
-        </footer>
     </body>
 </html>
